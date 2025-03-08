5ff6dac683e4adb8468e3673d5909ba90adb14f1
tincenma <temirlanilyasov1@gmail.com>
2025-03-08 20:52:03 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import blackSquare from "./black-square.jpg";
+
+function App() {
+  return (
+    <img src={blackSquare} alt="Black Square" className="square" />
+  );
+}
+
+export default App;
